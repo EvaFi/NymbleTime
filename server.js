40efa99bb195e74128a0921e0d968980ee685b7a
Fischer, Eva <eva.fischer@elekta.com>
2015-04-29 11:12:02 +0200

    Add winston logging to hapi.

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 var Hapi = require('hapi');
+var GoodWinston = require('good-winston');
+var winston = require('winston');
 'use strict';
 
 var server = new Hapi.Server();
+
+server.register({
+register: require('good'),
+  options: {
+    reporters: [
+      new GoodWinston({
+        ops: '*',
+        request: '*',
+        response: '*',
+        log: '*',
+        error: '*'
+      }, winston)
+    ]
+  }
+}, function(err) {
+  if(err) {
+    return server.log(['error'],'good load error: ' + err);
+  }
+});
+
 if( process.env.PORT ) {
   server.connection({ port: process.env.PORT });
 }
